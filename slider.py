91197f178008e820792d420368f8e5932674560b
Peter Vida <peter.vida@atos.net>
2020-10-20 22:22:00 +0200

    add mypy as dependency, fix some typing issues, output to console as well as file

--- slider.py
+++ slider.py
@@ -4,24 +4,25 @@ import pandas
 from tabulate import tabulate
 
 
-def main(args):
+def main(args: argparse.Namespace) -> None:
     df = pandas.read_csv(args.data)
 
-    df['week'] = df.index + 1
+    df['week'] = df.index.map(lambda i: i+1)
     
     labels = df.columns.tolist()
     labels.insert(0, labels.pop())
     df = df[labels]
     
     df['diff'] = df.apply(lambda row: row.spent - row.total, axis=1)
-    df['cum_diff'] = df['diff'].cumsum()
+    df['cum_diff'] = df['diff'].cumsum() # type: ignore
     
     with open(args.output, 'w') as out:
-        out.write(
-            tabulate(df,
-                     tablefmt=args.format,
-                     showindex='never',
-                     headers='keys'))
+        output = tabulate(df,
+                          tablefmt=args.format,
+                          showindex='never',
+                          headers='keys')
+        out.write(output)
+        print(output)
 
 
 if __name__ == '__main__':
